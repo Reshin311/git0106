2613e2273d655b4eb9030b944095b4f1f64c224c
Reshin311 <311reshin@gmail.com>
2021-01-06 20:26:37 +0900

    reshinを追加

--- git0106/ViewController.swift
+++ git0106/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("reshin")
+        
+        
+        
+        
+        
+        
     }
 
 
