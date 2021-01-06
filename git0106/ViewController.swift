b03e506af3c5480b04aa840a9e767f1053870013
Reshin311 <311reshin@gmail.com>
2021-01-06 20:22:54 +0900

    Initial Commit

---
+++ git0106/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git0106
+//
+//  Created by Shin on 2021/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
